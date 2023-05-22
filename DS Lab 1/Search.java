83609e4df87ae7d6f4f0c869de222c8f7f56f239
Coderrrr00550055 <105787610+Coderrrr00550055@users.noreply.github.com>
2023-05-23 00:33:52 +0530

    first commit

---
+++ DS Lab 1/Search.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+interface Search extends Remote
+{
+ // Declaring the method prototype
+ public String query(String search) throws RemoteException;
+}
